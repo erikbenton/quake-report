cbee2750620a29af1d301091b2705e72e3eb58ab
erikbenton <erik.benton@hotmail.com>
2018-04-10 11:51:20 -0400

    adding visual for preference in settings and updating after entering new value

--- app/src/main/java/com/example/android/quakereport/SettingsActivity.java
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -1,6 +1,10 @@
 package com.example.android.quakereport;
 
+import android.content.SharedPreferences;
+import android.media.audiofx.BassBoost;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,12 +20,36 @@ public class SettingsActivity extends AppCompatActivity
     }
 
     public static class EarthquakePreferenceFragment extends PreferenceFragment
+                                                     implements Preference.OnPreferenceChangeListener
     {
         @Override
         public void onCreate(@Nullable Bundle savedInstanceState)
         {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
+
+            Preference minMag = findPreference(getString(R.string.settings_min_magnitude_key));
+            bindPreferenceSummaryToValue(minMag);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue)
+        {
+            String stringValue = newValue.toString();
+            preference.setSummary(stringValue);
+            return true;
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference)
+        {
+            // Set listener on preference
+            preference.setOnPreferenceChangeListener(this);
+
+            // Get the preferences
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
         }
     }
 }
